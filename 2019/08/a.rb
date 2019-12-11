eadf19d79838c16bc65b45550eb6d6f663195385
Ryan Young <ryoung786@gmail.com>
2019-12-11 01:40:54 -0500

    day 8 part 2

--- 2019/08/a.rb
+++ 2019/08/a.rb
@@ -12,3 +12,20 @@ end
 layer = input.chars.each_slice(25*6).to_a[min_zeros_index]
 freqs = layer.reduce(Hash.new(0)) {|h,x| h[x]+=1; h}
 p freqs['1'] * freqs['2']
+
+
+
+final = ('2' * (25*6)).chars.map(&:to_i)
+layers = input.chars.map(&:to_i).each_slice(25*6).to_a
+foo = layers.reduce(final) do |acc, layer|
+  acc.each_with_index.map do |x,i| 
+    (x==2) ? layer[i] : x
+  end
+end
+
+
+
+
+foo.each_slice(25) do |x|
+  p (x.join('').gsub('0',' ').gsub('1','*')) # KCGEC
+end
