20b32f3394fab3ceb4487086dfb96de1e4d0cf18
Ryan Young <ryany@spotify.com>
2021-01-07 03:09:18 -0500

    day 4

---
+++ 2019/elixir/test/days/d_04_test.exs
@@ -0,0 +1,22 @@
+defmodule Days.D_04_Test do
+  use ExUnit.Case
+  import Days.D_04
+  import Enum
+
+  test "a" do
+    n = 111_111
+    assert has_two_adj_digits(n) and digits_never_decrease(n)
+
+    n = 223_450
+    assert has_two_adj_digits(n) and not digits_never_decrease(n)
+
+    n = 123_789
+    assert not has_two_adj_digits(n) and digits_never_decrease(n)
+  end
+
+  test "b" do
+    assert strict_duplicate(112_233)
+    assert not strict_duplicate(123_444)
+    assert strict_duplicate(111_122)
+  end
+end
