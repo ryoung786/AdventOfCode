a66cb513d72d2e6e774e78b77d252e4b0442f7b3
Ryan Young <ryany@spotify.com>
2021-01-10 16:10:05 -0500

    day 9

--- 2019/elixir/lib/days/09/d_09.ex
+++ 2019/elixir/lib/days/09/d_09.ex
@@ -2,18 +2,16 @@ defmodule Days.D_09 do
   use Days.Base
 
   @impl true
-  def part_one(str) do
-    str |> Util.to_intcode_program() |> boost_keycode()
-  end
+  def part_one(str), do: str |> Util.to_intcode_program() |> run_boost(:test)
 
   @impl true
-  def part_two(str) do
-    # str |> Util.to_intcode_program() |> boost_keycode()
-  end
+  def part_two(str), do: str |> Util.to_intcode_program() |> run_boost(:sensor_boost)
 
-  def boost_keycode(prog) do
-    {:ok, vm} = Intcode.new(prog, [1])
-    state = Intcode.run_sync(vm) |> IO.inspect(label: "[state] ")
-    state.output
+  def run_boost(prog, mode) do
+    input_val = %{test: 1, sensor_boost: 2} |> Map.get(mode)
+
+    {:ok, vm} = Intcode.new(prog, [input_val])
+    %{output: [result | _]} = Intcode.run_sync(vm)
+    result
   end
 end
