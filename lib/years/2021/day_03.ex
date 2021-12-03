76cdaeb47608c8cf6ecba72f027d702858225c68
Ryan Young <ryoung786@gmail.com>
2021-12-03 02:35:41 -0500

    day3 cleanup - still bad though

--- lib/years/2021/day_03.ex
+++ lib/years/2021/day_03.ex
@@ -2,64 +2,57 @@ defmodule Aoc.Year2021.Day03 do
   use Aoc.DayBase
 
   def part_one(input) do
-    [a | _] = entries = input |> Input.to_str_list()
-    bitmask = Integer.pow(2, String.length(a)) - 1
+    transposed =
+      input
+      |> Input.to_str_list()
+      |> Enum.map(&String.graphemes/1)
+      |> transpose()
 
-    gamma =
-      0..(String.length(a) - 1)
-      |> Enum.map_join(fn col ->
-        column(entries, col)
-      end)
-      |> Integer.parse(2)
-      |> elem(0)
-
-    epsilon = Bitwise.bxor(gamma, bitmask)
+    gamma = greek_rate(transposed, &Enum.max_by/2)
+    epsilon = greek_rate(transposed, &Enum.min_by/2)
 
     gamma * epsilon
   end
 
-  def part_two(input) do
-    [a | _] = entries = input |> Input.to_str_list()
+  defp transpose(m), do: m |> Enum.zip() |> Enum.map(fn tup -> Tuple.to_list(tup) end)
+  defp base2to10(str), do: Integer.parse(str, 2) |> elem(0)
 
-    {o2, _} =
-      0..(String.length(a) - 1)
-      |> Enum.reduce_while(entries, fn col, nums ->
-        rating = column(nums, col, :o2)
-
-        remaining = nums |> match_rating(rating, col)
-
-        case remaining do
-          [result] -> {:halt, result}
-          _ -> {:cont, remaining}
-        end
-      end)
-      |> Integer.parse(2)
-
-    {co2, _} =
-      0..(String.length(a) - 1)
-      |> Enum.reduce_while(entries, fn col, nums ->
-        rating = column(nums, col, :co2)
-
-        remaining = nums |> match_rating(rating, col)
-
-        case remaining do
-          [result] -> {:halt, result}
-          _ -> {:cont, remaining}
-        end
-      end)
-      |> Integer.parse(2)
-
-    o2 * co2
+  defp greek_rate(transposed, f) do
+    transposed
+    |> Enum.map_join(fn row ->
+      {k, _v} = row |> Enum.frequencies() |> f.(fn {_, v} -> v end)
+      k
+    end)
+    |> base2to10()
   end
 
-  def match_rating(nums, rating, col) do
+  def part_two(input) do
+    entries = input |> Input.to_str_list()
+    calculate_rating(entries, :o2) * calculate_rating(entries, :co2)
+  end
+
+  defp calculate_rating(entries, rating) do
+    0..99_999_999
+    |> Enum.reduce_while(entries, fn col, nums ->
+      digit = column(nums, col, rating)
+
+      remaining = nums |> match_rating(digit, col)
+
+      case remaining do
+        [result] -> {:halt, result}
+        _ -> {:cont, remaining}
+      end
+    end)
+    |> base2to10()
+  end
+
+  defp match_rating(nums, rating, col) do
     Enum.filter(nums, fn num ->
       String.at(num, col) == rating
     end)
   end
 
-  # returns {most_frequent, least_frequent}
-  def column(numbers, col, rating \\ :o2) do
+  defp column(numbers, col, rating) do
     freqs =
       numbers
       |> Enum.map(&String.at(&1, col))
