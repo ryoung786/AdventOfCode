979592ddd8fdae174ae592fa211ead8c95cc89ff
Ryan Young <ryoung786@gmail.com>
2021-12-03 16:10:17 -0500

    more concise day3

--- lib/years/2021/day_03.ex
+++ lib/years/2021/day_03.ex
@@ -2,66 +2,56 @@ defmodule Aoc.Year2021.Day03 do
   use Aoc.DayBase
 
   def part_one(input) do
-    transposed =
+    gamma =
       input
       |> Input.to_str_list()
       |> Enum.map(&String.graphemes/1)
       |> transpose()
+      |> Enum.map_join(&most_common_digit(&1))
+      |> base2to10()
 
-    gamma = greek_rate(transposed, &Enum.max_by/2)
-    epsilon = greek_rate(transposed, &Enum.min_by/2)
+    epsilon = Bitwise.bxor(gamma, 0b111111111111)
 
     gamma * epsilon
+  end
+
+  def part_two(input) do
+    entries = input |> Input.to_str_list()
+    rating(entries, :o2) * rating(entries, :co2)
   end
 
   defp transpose(m), do: m |> Enum.zip() |> Enum.map(fn tup -> Tuple.to_list(tup) end)
   defp base2to10(str), do: Integer.parse(str, 2) |> elem(0)
 
-  defp greek_rate(transposed, f) do
-    transposed
-    |> Enum.map_join(fn row ->
-      {k, _v} = row |> Enum.frequencies() |> f.(fn {_, v} -> v end)
-      k
-    end)
-    |> base2to10()
-  end
+  defp rating(numbers, bit_criteria),
+    do: reduce_to_one_number(numbers, bit_criteria) |> base2to10()
 
-  def part_two(input) do
-    entries = input |> Input.to_str_list()
-    calculate_rating(entries, :o2) * calculate_rating(entries, :co2)
-  end
+  defp reduce_to_one_number(numbers, bit_criteria) do
+    0..999
+    |> Enum.reduce_while(numbers, fn bit_position, numbers ->
+      digit =
+        numbers
+        |> Enum.map(&String.at(&1, bit_position))
+        |> most_common_digit(bit_criteria)
 
-  defp calculate_rating(entries, rating) do
-    0..99_999_999
-    |> Enum.reduce_while(entries, fn col, nums ->
-      digit = column(nums, col, rating)
-
-      remaining = nums |> match_rating(digit, col)
-
-      case remaining do
+      case numbers |> match_digit(bit_position, digit) do
         [result] -> {:halt, result}
-        _ -> {:cont, remaining}
+        remaining -> {:cont, remaining}
       end
     end)
-    |> base2to10()
   end
 
-  defp match_rating(nums, rating, col) do
+  defp match_digit(nums, bit_position, digit) do
     Enum.filter(nums, fn num ->
-      String.at(num, col) == rating
+      String.at(num, bit_position) == digit
     end)
   end
 
-  defp column(numbers, col, rating) do
-    freqs =
-      numbers
-      |> Enum.map(&String.at(&1, col))
-      |> Enum.frequencies()
+  defp most_common_digit(lst, bit_criteria \\ :o2) do
+    zeros = Enum.frequencies(lst) |> Map.get("0", 0)
+    ones = Enum.frequencies(lst) |> Map.get("1", 0)
 
-    zeros = Map.get(freqs, "0", 0)
-    ones = Map.get(freqs, "1", 0)
-
-    if rating == :o2 do
+    if bit_criteria == :o2 do
       if ones >= zeros, do: "1", else: "0"
     else
       if ones >= zeros, do: "0", else: "1"
