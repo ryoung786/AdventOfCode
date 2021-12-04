dd0330e7af3f08609b9ef60278bc550be749ddac
Ryan Young <ryoung786@gmail.com>
2021-12-04 02:35:35 -0500

    fix problem::solution links

--- lib/aoc_web/live/components.ex
+++ lib/aoc_web/live/components.ex
@@ -14,30 +14,19 @@ defmodule AocWeb.Components do
     """
   end
 
-  defp gh_path(year, day) when year in ~w(2021 2020 2019) do
-    root_url = "https://github.com/ryoung786/AdventOfCode"
-
-    gh_base =
-      case year do
-        "2021" -> "#{root_url}/blob/main/2021/lib/days/"
-        "2020" -> "#{root_url}/blob/main/2020/lib/days/"
-        "2019" -> "#{root_url}/blob/main/2019/elixir/lib/days/"
-      end
-
-    day_str = String.pad_leading(day, 2, "0")
+  defp gh_path(year, day) when year in [2021, 2020, 2019] do
+    day = String.pad_leading("#{day}", 2, "0")
 
     if String.to_integer(day) in 1..25,
-      do: gh_base <> "#{day_str}/d_#{day_str}.ex",
+      do: "https://github.com/ryoung786/AdventOfCode/blob/main/lib/years/#{year}/day_#{day}.ex",
       else: "/"
   end
 
   defp gh_path(_year, _day), do: "/"
 
-  defp official_site(year, day) when year in ~w(2021 2020 2019) do
-    url_base = "https://adventofcode.com/#{year}/day/"
-
-    if String.to_integer(day) in 1..25,
-      do: url_base <> day,
+  defp official_site(year, day) when year in [2021, 2020, 2019] do
+    if day in 1..25,
+      do: "https://adventofcode.com/#{year}/day/#{day}",
       else: "/"
   end
 
