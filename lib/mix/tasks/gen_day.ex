77be88ec0421b8a69dc2dd67d238048d89935a79
Ryan Young <ryoung786@gmail.com>
2021-12-12 19:54:56 -0500

    mix gen_day works

--- lib/mix/tasks/gen_day.ex
+++ lib/mix/tasks/gen_day.ex
@@ -9,25 +9,25 @@ defmodule Mix.Tasks.GenDay do
       )
 
     assigns = %{
-      "year" => Keyword.get(opts, :year, 2021),
-      "day" => Keyword.get(opts, :day) |> String.pad_leading(2)
+      year: Keyword.get(opts, :year, 2021) |> Integer.to_string(),
+      day: Keyword.get(opts, :day) |> Integer.to_string() |> String.pad_leading(2)
     }
 
     # lib/aoc/years/2021/day_01.ex
     Mix.Generator.copy_template(
       Path.join([:code.priv_dir(:aoc), "templates", "day.ex.eex"]),
-      Path.join(["lib", "aoc", "years", assigns["year"], "day_#{assigns["day"]}.ex"]),
+      Path.join(["lib", "aoc", "years", assigns.year, "day_#{assigns.day}.ex"]),
       assigns |> Enum.into([])
     )
 
     # test/aoc/years/2021/day_01_test.exs
     Mix.Generator.copy_template(
       Path.join([:code.priv_dir(:aoc), "templates", "day_test.exs.eex"]),
-      Path.join(["test", "aoc", "years", assigns["year"], "day_#{assigns["day"]}_test.exs"]),
+      Path.join(["test", "aoc", "years", assigns.year, "day_#{assigns.day}_test.exs"]),
       assigns |> Enum.into([])
     )
 
     # priv/01.input
-    Mix.Generator.create_file(Path.join(["priv", assigns["year"], "#{assigns["day"]}.input"]), "")
+    Mix.Generator.create_file(Path.join(["priv", assigns.year, "#{assigns.day}.input"]), "")
   end
 end
