d7a8617ac1f26a345bd7ae5168bc222cafb05c0c
Ryan Young <ryoung786@gmail.com>
2021-12-12 20:50:41 -0500

    webpage uses years dir instead of hardcoded year values

--- lib/aoc_web/live/page_live.ex
+++ lib/aoc_web/live/page_live.ex
@@ -3,13 +3,14 @@ defmodule AocWeb.PageLive do
 
   @impl true
   def mount(params, _session, socket) do
+    years = File.ls!(Path.join(["lib", "aoc", "years"])) |> Enum.sort()
     year = Map.get(params, "year", "2021")
-    {:ok, assign(socket, year: year)}
+    {:ok, assign(socket, years: years, selected_year: year)}
   end
 
   @impl true
   def handle_params(params, _uri, socket) do
     year = Map.get(params, "year", "2021")
-    {:noreply, assign(socket, year: year)}
+    {:noreply, assign(socket, selected_year: year)}
   end
 end
