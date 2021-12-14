044dd3c8825435cf6c7361ca4b18f5c32e08c2b6
Ryan Young <ryoung786@gmail.com>
2021-12-14 01:16:33 -0500

    almost got day14

---
+++ lib/aoc/years/2021/day_14.ex
@@ -0,0 +1,74 @@
+defmodule Aoc.Year2021.Day14 do
+  use Aoc.DayBase
+
+  def part_one(input) do
+    {tpl, rules} = parse_input(input)
+
+    {{_min, min_val}, {_max, max_val}} =
+      1..10
+      |> Enum.reduce(tpl, fn _, tpl -> step(tpl, rules) end)
+      |> Enum.frequencies()
+      |> IO.inspect(label: "freq")
+      |> Enum.min_max_by(fn {_k, v} -> v end)
+
+    max_val - min_val
+  end
+
+  def part_two(input) do
+    {tpl, rules} = parse_input(input)
+
+    every_pair_after_20 =
+      Enum.reduce(Map.keys(rules), %{}, fn pair, acc ->
+        freqs =
+          1..20
+          |> Enum.reduce(String.split(pair, "", trim: true), fn _, tpl ->
+            step(tpl, rules)
+          end)
+          |> Enum.frequencies()
+
+        IO.puts("added #{pair}")
+        Map.put(acc, pair, freqs)
+      end)
+
+    tpl_after_20 =
+      1..20
+      |> Enum.reduce(tpl, fn i, tpl ->
+        IO.puts("step #{i}")
+        step(tpl, rules)
+      end)
+
+    IO.puts("we have every_pair_after_20 and tpl_after_20")
+
+    {{_min, min_val}, {_max, max_val}} =
+      tpl_after_20
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.reduce(%{}, fn [a, b], acc ->
+        Map.merge(acc, every_pair_after_20[a <> b], fn _k, v1, v2 -> v1 + v2 end)
+      end)
+      |> IO.inspect(label: "final frequencies")
+      |> Enum.min_max_by(fn {_k, v} -> v end)
+      |> IO.inspect(label: "min, max")
+
+    max_val - min_val
+  end
+
+  def parse_input(input) do
+    [tpl, rules] = String.split(input, "\n\n", trim: true)
+
+    {
+      tpl |> String.graphemes(),
+      rules
+      |> Input.to_str_list()
+      |> Enum.reduce(%{}, fn line, acc ->
+        [pair, insertion] = String.split(line, " -> ", trim: true)
+        Map.put(acc, pair, insertion)
+      end)
+    }
+  end
+
+  def step([first_char | _] = tpl, rules) do
+    tpl = Enum.chunk_every(tpl, 2, 1, :discard)
+
+    [first_char | Enum.flat_map(tpl, fn [a, b] -> [rules[a <> b], b] end)]
+  end
+end
---
+++ test/aoc/years/2021/day_14_test.exs
@@ -0,0 +1,39 @@
+defmodule Aoc.Year2021.Day14Test do
+  use ExUnit.Case, async: true
+  import Aoc.Year2021.Day14
+
+  setup_all do
+    input = """
+    NNCB
+
+    CH -> B
+    HH -> N
+    CB -> H
+    NH -> C
+    HB -> C
+    HC -> B
+    HN -> C
+    NN -> C
+    BH -> H
+    NC -> B
+    NB -> B
+    BN -> B
+    BB -> N
+    BC -> B
+    CC -> N
+    CN -> C
+    """
+
+    {:ok, input: input}
+  end
+
+  describe "2021 Day 14" do
+    test "part one", %{input: input} do
+      assert part_one(input) == 1588
+    end
+
+    test "part two", %{input: input} do
+      assert part_two(input) == 2_188_189_693_529
+    end
+  end
+end
