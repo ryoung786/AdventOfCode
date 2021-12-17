f18589ac0899a7c34afbbd652a4906918d08427d
Ryan Young <ryoung786@gmail.com>
2021-12-17 14:22:43 -0500

    day 17 cleanup

--- lib/aoc/years/2021/day_17.ex
+++ lib/aoc/years/2021/day_17.ex
@@ -2,83 +2,62 @@ defmodule Aoc.Year2021.Day17 do
   use Aoc.DayBase
 
   def part_one(input) do
-    {xtarget, ytarget} = _target = to_target_range(input)
-
-    find_valid_slopes(xtarget, ytarget)
+    input
+    |> to_target_range()
+    |> find_valid_slopes()
+    |> highest_position()
   end
 
   def part_two(input) do
-    {xtarget, ytarget} = _target = to_target_range(input)
-
-    find_valid_slopes2(xtarget, ytarget)
+    input
+    |> to_target_range()
+    |> find_valid_slopes()
+    |> Enum.count()
   end
 
   def to_target_range(str) do
     [_, xrange, yrange] = Regex.run(~r/target area: x=(.*), y=(.*)$/, str)
-
-    [ymin, ymax] = String.split(yrange, "..") |> Enum.map(&String.to_integer/1)
-    [xmin, xmax] = String.split(xrange, "..") |> Enum.map(&String.to_integer/1)
-
-    {xmin..xmax, ymin..ymax}
+    {Code.eval_string(xrange) |> elem(0), Code.eval_string(yrange) |> elem(0)}
   end
 
-  def in_target?(xtarget, ytarget, {x, y}) do
-    x in xtarget and y in ytarget
-  end
+  def in_target?(xtarget, ytarget, {x, y}), do: x in xtarget and y in ytarget
+  def beyond_target?(_..xmax, ymin.._, {x, y}), do: x > xmax or y < ymin
 
-  def beyond_target?(_..xmax, ymin.._, {x, y}) do
-    x > xmax or y < ymin
-  end
-
-  def find_valid_slopes(_xmin..xmax = xtarget, _ymin.._ymax = ytarget) do
-    for dx <- 1..(div(xmax, 2) + 1), dy <- 0..50 do
+  def find_valid_slopes({_xmin..xmax = xtarget, ymin.._ymax = ytarget}) do
+    for dx <- 1..xmax, dy <- ymin..100 do
       points = plot_course(dx, dy, xtarget, ytarget)
-
-      if Enum.any?(points, fn point -> in_target?(xtarget, ytarget, point) end) do
-        Enum.max_by(points, fn {_x, y} -> y end) |> elem(1)
-      else
-        -999_999_999_999_999_999_999_999
-      end
+      {dx, dy, points}
     end
+    |> Enum.filter(fn {_dx, _dy, points} ->
+      Enum.any?(points, fn point -> in_target?(xtarget, ytarget, point) end)
+    end)
+  end
+
+  def highest_position(shots) do
+    shots
+    |> Enum.map(fn {_, _, points} ->
+      {_highest_x, highest_y} = Enum.max_by(points, fn {_x, y} -> y end)
+      highest_y
+    end)
     |> Enum.max()
   end
 
-  def find_valid_slopes2(_xmin..xmax = xtarget, ymin.._ymax = ytarget) do
-    for dx <- 1..xmax, dy <- ymin..100 do
-      points = plot_course(dx, dy, xtarget, ytarget)
-
-      if Enum.any?(points, fn point -> in_target?(xtarget, ytarget, point) end) do
-        {dx, dy}
-      else
-        :nope
-      end
-    end
-    |> Enum.reject(fn val -> val == :nope end)
-    |> IO.inspect(label: "valid ")
-    |> Enum.count()
-  end
-
   def plot_course(dx, dy, xtarget, ytarget) do
-    1..99_999_999_999_999_999
+    1..999
     |> Enum.reduce_while([{0, 0, dx, dy}], fn _, [{x, y, dx, dy} | _] = acc ->
-      x = x + dx
-      y = y + dy
-
-      dx =
-        cond do
-          dx > 0 -> dx - 1
-          dx < 0 -> dx + 1
-          dx == 0 -> 0
-        end
+      {x, y} = {x + dx, y + dy}
 
       dy = dy - 1
+      dx = update_x_velocity(dx)
 
-      if beyond_target?(xtarget, ytarget, {x, y}) do
-        {:halt, acc}
-      else
-        {:cont, [{x, y, dx, dy} | acc]}
-      end
+      if beyond_target?(xtarget, ytarget, {x, y}),
+        do: {:halt, acc},
+        else: {:cont, [{x, y, dx, dy} | acc]}
     end)
     |> Enum.map(fn {x, y, _, _} -> {x, y} end)
   end
+
+  def update_x_velocity(0), do: 0
+  def update_x_velocity(dx) when dx > 0, do: dx - 1
+  def update_x_velocity(dx) when dx < 0, do: dx + 1
 end
