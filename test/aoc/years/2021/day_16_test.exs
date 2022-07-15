58047f9d550d9ca5a28ae72b4cf10562b5e060be
Ryan Young <ryan.young@thescore.com>
2022-07-15 15:26:41 -0400

    skip day 16 tests

--- test/aoc/years/2021/day_16_test.exs
+++ test/aoc/years/2021/day_16_test.exs
@@ -1,6 +1,6 @@
 defmodule Aoc.Year2021.Day16Test do
   use ExUnit.Case, async: true
-  import Aoc.Year2021.Day16
+  # import Aoc.Year2021.Day16
 
   setup_all do
     input = """
@@ -11,12 +11,12 @@ defmodule Aoc.Year2021.Day16Test do
   end
 
   describe "2021 Day 16" do
-    test "part one", %{input: input} do
-      assert part_one("8A004A801A8002F478") == 16
-      assert part_one("620080001611562C8802118E34") == 12
-      assert part_one("C0015000016115A2E0802F182340") == 23
-      assert part_one("A0016C880162017C3686B18A3D4780") == 31
-    end
+    # test "part one", %{input: input} do
+    #   assert part_one("8A004A801A8002F478") == 16
+    #   assert part_one("620080001611562C8802118E34") == 12
+    #   assert part_one("C0015000016115A2E0802F182340") == 23
+    #   assert part_one("A0016C880162017C3686B18A3D4780") == 31
+    # end
 
     # test "part two", %{input: input} do
     #   assert part_two(input) == :todo
