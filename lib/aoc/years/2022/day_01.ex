298af341b1c10ff39a52029e52ef846b547c6e0f
Ryan Young <ryoung786@gmail.com>
2022-12-01 01:31:42 -0500

    2022 day 1

--- lib/aoc/years/2022/day_01.ex
+++ lib/aoc/years/2022/day_01.ex
@@ -3,15 +3,28 @@ defmodule Aoc.Year2022.Day01 do
 
   def part_one(input) do
     input
-    |> Input.to_str_list()
-
-    "TBD"
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.max()
   end
 
   def part_two(input) do
     input
-    |> Input.to_str_list()
-
-    "TBD"
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 end
